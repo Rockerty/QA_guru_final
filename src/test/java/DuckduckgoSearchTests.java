96e1bdd40ed6094f5e20630d41ec1c1bb9b26bed
NSmirnov <NSmirnov@it-one.ru>
2026-02-05 08:32:01 +0300

    Conflict added

--- src/test/java/DuckduckgoSearchTests.java
+++ src/test/java/DuckduckgoSearchTests.java
@@ -4,6 +4,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class DuckduckgoSearchTests {
     @Test
@@ -15,7 +16,7 @@ public class DuckduckgoSearchTests {
         $("[name=q]").shouldBe(visible).click();
         //sleep(500);
         $("[name=q]").setValue("selenide");
-        //sleep(2000);
+        sleep(2000);
         $("[name=q]").pressEnter();
         $("[data-testid=result]").shouldHave(text("selenide.org"));
     }
