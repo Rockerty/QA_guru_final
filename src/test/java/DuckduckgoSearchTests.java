8fafd26946d83d95db98b8c4a19467fd3a659612
NSmirnov <NSmirnov@it-one.ru>
2026-02-05 08:13:53 +0300

    conflict created

--- src/test/java/DuckduckgoSearchTests.java
+++ src/test/java/DuckduckgoSearchTests.java
@@ -4,13 +4,14 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class DuckduckgoSearchTests {
     @Test
     void successfulSearchTest() {
         open("https://duckduckgo.com");
         // Пауза для "чтения" страницы
-        //sleep(2000);
+        sleep(2000);
         // Медленно вводим текст
         $("[name=q]").shouldBe(visible).click();
         //sleep(500);
