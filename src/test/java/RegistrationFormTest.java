59df3c79bf8557a758d253d4d743ee352442f16b
RockertyLenovo\Admin <parchipacoza@mail.ru>
2026-02-21 11:45:33 +0300

    PageObject added

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -1,15 +1,15 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import pages.RegistrationFormPage;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static testdata.TestData.*;
 
 public class RegistrationFormTest {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
 
     @BeforeAll
     static void allTestsSetUp() {
@@ -18,125 +18,101 @@ public class RegistrationFormTest {
         Configuration.baseUrl = "https://app.qa.guru/";
     }
 
+    @BeforeEach
+    void setUp() {
+        registrationFormPage.openPage();
+        registrationFormPage.advertisingMenuClose();
+    }
+
     @Test
     void successfulRegistrationTest() {
-        open("automation-practice-form/");
-        $x("//*[@data-testid='ClearIcon']").shouldBe(visible).click();
-        $x("//input[@data-testid='firstName']").setValue(firstName);
-        $x("//input[@data-testid='lastName']").setValue(lastName);
-        $x("//input[@data-testid='email']").setValue(email);
-        $x("//input[@data-testid='phone']").setValue(phone);
-        $x("//*[contains(text(), 'Language')]/following::*[@role='combobox'][1]").shouldBe(visible).click();
-        $x("//*[@data-value='" + language + "']").shouldBe(visible).click();
-        // Выбор даты рождения
-        $x("//*[@data-testid='CalendarIcon']").scrollTo().shouldBe(visible).click();
-        // Год
-        $x("//*[contains(@class, 'MuiPickersCalendarHeader-switchViewButton')]").shouldBe(visible).click();
-        $x("//button[text()='" + birthYear + "']").scrollTo().shouldBe(visible).click();
-        // Месяц
-        $x("//button[text()='" + birthMonth + "']").scrollTo().shouldBe(visible).click();
-        // День
-        $x("//button[text()='" + birthDay + "']").scrollTo().shouldBe(visible).click();
-        // Выбор пола
-        $x("//input[@data-testid='gender' and @value='" + gender + "']").click();
-        // Выбор хобби
+        registrationFormPage.enterField("firstName", firstName);
+        registrationFormPage.enterField("lastName", lastName);
+        registrationFormPage.enterField("email", email);
+        registrationFormPage.enterField("phone", phone);
+        registrationFormPage.openCombobox("Language");
+        registrationFormPage.languageChoice(language);
+        registrationFormPage.setBirthDate(birthYear, birthMonth, birthDay);
+        registrationFormPage.genderChoice(gender);
         for (String hobby : hobbies) {
-            $x("//input[@value='" + hobby + "' and @data-testid='hobbies']").click();
+            registrationFormPage.hobbyChoice(hobby);
         }
-        // Выбор предметов
-        $x("//*[contains(text(), 'Subject')]/following::*[@role='combobox'][1]").scrollTo().click();
+        registrationFormPage.openCombobox("Subject");
         for (String subject : subjects) {
-            $x("//*[@role='listbox']//*[@data-value='" + subject + "']").shouldBe(visible).click();
+            registrationFormPage.selectOption(subject);
         }
         actions().sendKeys(Keys.ESCAPE).perform();
-        // Выбор страны, города, адреса
-        $x("//*[contains(text(), 'State')]/following::*[@role='combobox'][1]").click();
-        $x("//*[@data-value='" + state + "']").shouldBe(visible).click();
-        $(By.id("city-select")).selectOption(city);
-        // Сдвиг ползунка
-        actions()
-                .clickAndHold($(By.xpath("//span[contains(@class, 'MuiSlider-thumb')]")))
-                .moveByOffset(100, 0)
-                .release()
-                .perform();
-        $(By.xpath("//textarea[@data-testid='address']")).setValue(address);
-        // Загрузка файла
-        $(By.xpath("//input[@type='file']")).uploadFromClasspath("Little poito art.png");
-        $x("//button[@type='submit']").scrollTo().click();
+        registrationFormPage.openCombobox("State");
+        registrationFormPage.stateChoice(state);
+        registrationFormPage.cityChoice(city);
+        registrationFormPage.sliderMove(100);
+        registrationFormPage.textAreaFill("address", address);
+        registrationFormPage.fileUpload("Little poito art.png");
+        registrationFormPage.clickSubmitButton();
         // Проверки
-        $(By.xpath("//p[text()='firstName']/following::p[1]")).scrollTo().shouldBe(visible).shouldHave(text(firstName));
-        $(By.xpath("//p[text()='lastName']/following::p[1]")).shouldHave(text(lastName));
-        $(By.xpath("//p[text()='email']/following::p[1]")).shouldHave(text(email));
-        $(By.xpath("//p[text()='gender']/following::p[1]")).shouldHave(text(gender));
-        $(By.xpath("//p[text()='phone']/following::p[1]")).shouldHave(text(phone));
-        $(By.xpath("//p[text()='dateOfBirth']/following::p[1]")).shouldHave(text(expectedBirthDate));
+        registrationFormPage.assertFieldOutput("firstName", firstName);
+        registrationFormPage.assertFieldOutput("lastName", lastName);
+        registrationFormPage.assertFieldOutput("email", email);
+        registrationFormPage.assertFieldOutput("phone", phone);
+        registrationFormPage.assertFieldOutput("gender", gender);
+        registrationFormPage.assertFieldOutput("stateCity", state);
+        registrationFormPage.assertFieldOutput("stateCity", city);
+        registrationFormPage.assertFieldOutput("language", language);
+        registrationFormPage.assertFieldOutput("address", address);
+        registrationFormPage.assertFieldOutput("dateOfBirth", expectedBirthDate);
         for (String subject : subjects) {
-            $(By.xpath("//p[text()='subjects']/following::p[1]")).shouldHave(text(subject));
+            registrationFormPage.assertFieldOutput("subjects", subject);
         }
         for (String hobby : hobbies) {
-            $(By.xpath("//p[text()='hobbies']/following::p[1]")).shouldHave(text(hobby));
+            registrationFormPage.assertFieldOutput("hobbies", hobby);
         }
-        $(By.xpath("//p[text()='stateCity']/following::p[1]")).shouldHave(text(state));
-        $(By.xpath("//p[text()='stateCity']/following::p[1]")).shouldHave(text(city));
-        $(By.xpath("//p[text()='language']/following::p[1]")).shouldHave(text(language));
-        $(By.xpath("//p[text()='address']/following::p[1]")).shouldHave(text(address));
     }
 
     @Test
     void requiredFieldsErrorTest() {
-        open("automation-practice-form/");
-        $x("//*[@data-testid='ClearIcon']").shouldBe(visible).click();
-        $x("//button[@type='submit']").scrollTo().click();
-        $(By.xpath("//form")).shouldHave(text("First Name is required"));
-        $(By.xpath("//form")).shouldHave(text("Last Name is required"));
-        $(By.xpath("//form")).shouldHave(text("E-mail is required"));
-        $(By.xpath("//form")).shouldHave(text("Phone number is required"));
-        $(By.xpath("//form")).shouldHave(text("Gender is required"));
+        registrationFormPage.clickSubmitButton();
+        registrationFormPage.registrationFormTextAssert("First Name is required");
+        registrationFormPage.registrationFormTextAssert("Last Name is required");
+        registrationFormPage.registrationFormTextAssert("E-mail is required");
+        registrationFormPage.registrationFormTextAssert("Phone number is required");
+        registrationFormPage.registrationFormTextAssert("Gender is required");
     }
 
     @Test
     void shortEmailTest() {
-        open("automation-practice-form/");
-        $x("//*[@data-testid='ClearIcon']").shouldBe(visible).click();
-        $x("//input[@data-testid='email']").setValue(shortIncorrectEmail);
-        $x("//button[@type='submit']").scrollTo().click();
-        $(By.xpath("//form")).shouldHave(text("E-mail must be at least 10 symbols long"));
+        registrationFormPage.enterField("email", shortIncorrectEmail);
+        registrationFormPage.clickSubmitButton();
+        registrationFormPage.registrationFormTextAssert("E-mail must be at least 10 symbols long");
     }
 
     @Test
     void invalidFileExtensionTest() {
-        open("automation-practice-form/");
-        $x("//*[@data-testid='ClearIcon']").shouldBe(visible).click();
-        $(By.xpath("//input[@type='file']")).uploadFromClasspath("VanyaVPN.exe");
-        $x("//button[@type='submit']").scrollTo().click();
-        $(By.xpath("//form")).shouldHave(text("Upload failed"));
-        $(By.xpath("//form")).shouldHave(text("Invalid extension"));
+        registrationFormPage.fileUpload("VanyaVPN.exe");
+        registrationFormPage.clickSubmitButton();
+        registrationFormPage.registrationFormTextAssert("Upload failed");
+        registrationFormPage.registrationFormTextAssert("Invalid extension");
     }
 
     @Test
     void invalidEmailTest() {
-        open("automation-practice-form/");
-        $x("//*[@data-testid='ClearIcon']").shouldBe(visible).click();
-        $x("//input[@data-testid='email']").setValue(incorrectFormatEmail);
-        $x("//button[@type='submit']").scrollTo().click();
-        $(By.xpath("//form")).shouldHave(text("E-mail is invalid"));
+        registrationFormPage.enterField("email", incorrectFormatEmail);
+        registrationFormPage.clickSubmitButton();
+        registrationFormPage.registrationFormTextAssert("E-mail is invalid");
     }
 
     @Test
     void requiredFieldsOnlyTest() {
-        open("automation-practice-form/");
-        $x("//*[@data-testid='ClearIcon']").shouldBe(visible).click();
-        $x("//input[@data-testid='firstName']").setValue(firstName);
-        $x("//input[@data-testid='lastName']").setValue(lastName);
-        $x("//input[@data-testid='email']").setValue(email);
-        $x("//input[@data-testid='phone']").setValue(phone);
-        $x("//input[@data-testid='gender' and @value='" + gender + "']").scrollTo().click();
-        $x("//button[@type='submit']").scrollTo().click();
+        registrationFormPage.enterField("firstName", firstName);
+        registrationFormPage.enterField("lastName", lastName);
+        registrationFormPage.enterField("email", email);
+        registrationFormPage.enterField("phone", phone);
+        registrationFormPage.genderChoice(gender);
+        registrationFormPage.clickSubmitButton();
         // Проверки
-        $(By.xpath("//p[text()='firstName']/following::p[1]")).scrollTo().shouldBe(visible).shouldHave(text(firstName));
-        $(By.xpath("//p[text()='lastName']/following::p[1]")).shouldHave(text(lastName));
-        $(By.xpath("//p[text()='email']/following::p[1]")).shouldHave(text(email));
-        $(By.xpath("//p[text()='phone']/following::p[1]")).shouldHave(text(phone));
-        $(By.xpath("//p[text()='gender']/following::p[1]")).shouldHave(text(gender));
+        registrationFormPage.assertFieldOutput("firstName", firstName);
+        registrationFormPage.assertFieldOutput("lastName", lastName);
+        registrationFormPage.assertFieldOutput("email", email);
+        registrationFormPage.assertFieldOutput("phone", phone);
+        registrationFormPage.assertFieldOutput("gender", gender);
     }
 }
---
+++ src/test/java/pages/RegistrationFormPage.java
@@ -0,0 +1,111 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import components.DatePickerComponent;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormPage {
+    //Elements
+    private SelenideElement submitButton = $x("//button[@type='submit']");
+    private DatePickerComponent datePicker = new DatePickerComponent();
+
+    private static String inputFieldXpathTemplate = "//input[@data-testid='%s']";
+    private static String outputFieldTemplate = "//p[text()='%s']/following::p[1]";
+    private static String comboboxTemplate = "//*[contains(text(), '%s')]/following::*[@role='combobox'][1]";
+    private static String optionTemplate = "//*[@role='listbox']//*[contains(text(), '%s')]";
+    private static String dataValueTemplate = "//*[@data-value='%s']";
+    private static String genderXpathTemplate = "//input[@data-testid='gender' and @value='%s']";
+    private static String hobbyXpathTemplate = "//input[@data-testid='hobbies' and @value='%s']";
+    private static String citySelect = "city-select";
+    private static String textAreaXpathTemplate = "//textarea[@data-testid='%s']";
+    private static String sliderXpath = "//span[contains(@class, 'MuiSlider-thumb')]";
+    private static String uploadFileXpath = "//input[@type='file']";
+    private static String registrationFormXpath = "//form";
+    private static String advertisingMenuCloseButtonXpath = "//*[@data-testid='ClearIcon']";
+
+    //Actions
+    public void openPage(){
+        open("automation-practice-form/");
+    }
+
+    public void clickSubmitButton() {
+        submitButton.scrollTo().click();
+    }
+
+    public void enterField(String fieldName, String value) {
+        String xpath = String.format(inputFieldXpathTemplate, fieldName);
+        $x(xpath).scrollTo().setValue(value);
+    }
+
+    public void assertFieldOutput(String fieldName, String expectedValue) {
+        String xpath = String.format(outputFieldTemplate, fieldName);
+        $x(xpath).scrollTo().shouldBe(visible).shouldHave(text(expectedValue));
+    }
+
+    public void genderChoice(String gender) {
+        String xpath = String.format(genderXpathTemplate, gender);
+        $x(xpath).scrollTo().click();
+    }
+
+    public void hobbyChoice(String hobby) {
+        String xpath = String.format(hobbyXpathTemplate, hobby);
+        $x(xpath).scrollTo().click();
+    }
+
+    public void openCombobox(String name) {
+        String xpath = String.format(comboboxTemplate, name);
+        $x(xpath).scrollTo().click();
+    }
+
+    public void selectOption(String optionValue) {
+        String xpath = String.format(optionTemplate, optionValue);
+        $x(xpath).shouldBe(visible).click();
+    }
+
+    public void languageChoice(String language) {
+        String xpath = String.format(dataValueTemplate, language);
+        $x(xpath).shouldBe(visible).click();
+    }
+
+    public void stateChoice(String state) {
+        String xpath = String.format(dataValueTemplate, state);
+        $x(xpath).shouldBe(visible).click();
+    }
+
+    public void cityChoice(String city) {
+        $(By.id(citySelect)).selectOption(city);
+    }
+
+    public void textAreaFill (String name, String text) {
+        String xpath = String.format(textAreaXpathTemplate, name);
+        $x(xpath).scrollTo().setValue(text);
+    }
+
+    public void sliderMove (int xOffset) {
+        actions()
+                .clickAndHold($(By.xpath(sliderXpath)))
+                .moveByOffset(xOffset, 0)
+                .release()
+                .perform();
+    }
+
+    public void fileUpload(String fileName) {
+        $x(uploadFileXpath).uploadFromClasspath(fileName);
+    }
+
+    public void setBirthDate(String year, String month, String day) {
+        datePicker.setDate(year, month, day);
+    }
+
+    public void registrationFormTextAssert(String expectedText) {
+        $x(registrationFormXpath).shouldHave(text(expectedText));
+    }
+
+    public void advertisingMenuClose() {
+        $x(advertisingMenuCloseButtonXpath).shouldBe(visible).click();
+    }
+}
