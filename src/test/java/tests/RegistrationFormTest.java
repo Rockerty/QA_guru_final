71c1b3d2e005a0c9cc16dab30108a60d24e2a5cc
RockertyLenovo\Admin <parchipacoza@mail.ru>
2026-03-31 19:47:41 +0300

    Remote tests in seleniod added

--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -22,6 +22,8 @@ public class RegistrationFormTest {
         Configuration.timeout = 10000;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://app.qa.guru/";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
