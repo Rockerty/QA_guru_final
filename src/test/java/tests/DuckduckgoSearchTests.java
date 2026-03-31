ef0c2859ff459e595819ec2e527c00c242fe3234
RockertyLenovo\Admin <parchipacoza@mail.ru>
2026-03-31 20:00:18 +0300

    Remote tests in seleniod added

--- src/test/java/tests/DuckduckgoSearchTests.java
+++ src/test/java/tests/DuckduckgoSearchTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -9,12 +11,20 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
 
 public class DuckduckgoSearchTests {
+
+    @BeforeAll
+    static void allTestsSetUp() {
+        System.out.println("beforeAll");
+        Configuration.timeout = 10000;
+        Configuration.browserSize = "1920x1080";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+
     @Test
     void successfulSearchTest() {
         open("https://duckduckgo.com");
-        // Пауза для "чтения" страницы
-        //sleep(2000);
-        // Медленно вводим текст
         $("[name=q]").shouldBe(visible).click();
         //sleep(5000);
         sleep(3000);
