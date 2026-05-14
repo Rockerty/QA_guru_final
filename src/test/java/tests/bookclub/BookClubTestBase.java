be07d302b60c83f15073ef18a847c2713d16031a
RockertyLenovo\Admin <parchipacoza@mail.ru>
2026-05-14 21:34:34 +0300

    BookClubTestBase modify for Jenkins

--- src/test/java/tests/bookclub/BookClubTestBase.java
+++ src/test/java/tests/bookclub/BookClubTestBase.java
@@ -6,8 +6,14 @@ import api.logout.LogoutApiClient;
 import api.registration.RegistrationApiClient;
 import api.update.UpdateUserApiClient;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import helpers.Attach;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BookClubTestBase {
 
@@ -24,7 +30,33 @@ public class BookClubTestBase {
         RestAssured.basePath = "/api/v1";
 
         // Настройка Selenide для UI
-        Configuration.baseUrl = "https://book-club.qa.guru";
-        Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.baseUrl = System.getProperty("baseUrl");;
+        Configuration.remote = System.getProperty("selenoidRemoteURL");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("isHeadless"));
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+    }
+
+    @AfterEach
+    void tearDown() {
+        Selenide.closeWebDriver();
+    }
+
+    @AfterEach
+    void addAttachment() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
